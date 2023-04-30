b10a3789373f3c7d582d2567562a422b14539267
Lutica_CANARD <presan100@gmail.com>
2023-04-30 22:23:12 +0900

    node

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,10 @@ import { createServer } from "http";
 import { WebSocket } from "ws";
 
 const app = express();
-const server = createServer(app);
-const wsServer = new WebSocket.Server({ server, path: "/cana_rin_lab_ws" });
 const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("RES!!!");
-});
-wsServer.on("connection", (ws: WebSocket) => {
-
-});
-
-server.listen(PORT, () => {
-    console.log(`Server is open... on: ${PORT}`);
 });

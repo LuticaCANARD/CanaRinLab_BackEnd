7032f0f9f673f582b9a4147229a91e45f46609eb
LuticaCANARD <presan100@gmail.com>
2023-05-02 15:54:52 +0900

    dsa

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -10,6 +10,33 @@ export const DBpool = new Pool({
     idleTimeoutMillis: 1000, // close idle clients after 1 second
     connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
 }) 
+export class RouteResult {
+    result:boolean;
+    returns:object;
+    error_code:null|number;
+    error_desc : null|string;
+    constructor(result:boolean,ret:object,error_code?:null|number,error_desc?:null|string)
+    {
+        this.result = result
+        this.returns = ret
+        this.error_code = error_code
+        this.error_desc = error_desc
+    }
+    toObject():object
+    {
+        let ret = {"result" : this.result}
+        if (this.result)
+        {
+            ret['ret'] = this.returns
+        }
+        else
+        {
+            ret['ret'] = {"desc":this.error_desc}
+            ret['error_code'] = this.error_code
+        }
+        return ret
+    }
+}
      
 
 export default DBpool
--- src/vrchat/vrc_controller.ts
+++ src/vrchat/vrc_controller.ts
@@ -5,11 +5,24 @@
 */
 import * as Utils from '../Utils/utils'
 export module VrcControl {
+    let db = Utils.DBpool
     export async function routeVrcRequest(request:object,route:string):Promise<object>{
-        let result = {}
-        let db = Utils.DBpool
-        
+        let result:object = {}
+        switch(route) 
+        {
+            case 'test':
+                result = new Utils.RouteResult(true,{'test':'open!'}).toObject()
+            case 'insert_user' :
+                result = new Utils.RouteResult(true,await insertUserData()).toObject()
+            default :
+                result = new Utils.RouteResult(false,{},-1,'There is no command for server!').toObject()
+        }
         
         return result
+    }
+    async function insertUserData():Promise<object>
+    {
+
+        return
     }
 }

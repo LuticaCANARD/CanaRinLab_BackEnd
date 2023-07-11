3a5b5b7236319b6fdc93eb079242717329151bff
Lutica_CANARD <80238084+LuticaCANARD@users.noreply.github.com>
2023-07-12 08:57:34 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import { createServer } from "http";
 import { WebSocket } from "ws";
 import { VrcRouter } from "./vrchat/vrc_controller"
 //import * as WebControl from "./web/web_controller"
-import { PrismaClient } from '@prisma/client'
+//import { PrismaClient } from '@prisma/client'
 import { Elysia,Context } from 'elysia'
 
-const prisma = new PrismaClient()
+//const prisma = new PrismaClient()
 
 const app = new Elysia();
 const PORT = process.env.PORT || 80;

0f8e1d273b3827aeb757bb7c93a7a038b7f401b6
Lutica_CANARD <presan100@gmail.com>
2023-07-11 23:55:47 +0900

    fix : move to Elysia

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,18 @@
-import express from "express";
 import { createServer } from "http";
 import { WebSocket } from "ws";
-import * as VrcControl from "./vrchat/vrc_controller"
+import {VrcRouter} from "./vrchat/vrc_controller"
 import * as WebControl from "./web/web_controller"
 import { PrismaClient } from '@prisma/client'
+import { Elysia,Context } from 'elysia'
+
 const prisma = new PrismaClient()
 
-const app = express();
-const server = createServer(app);
-const wsServer = new WebSocket.Server({ server, path: "/cana_rin_lab_ws" });
+const app = new Elysia();
 const PORT = process.env.PORT || 80;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.get("/", async (req, res) => {
 
-    res.send("RES!!!");
-});
-app.get('/vrchat/:order',async(req, res) => {
-    try{
-        let request = req.query;
-        let result = await VrcControl.VrcControl.routeVrcRequest(request,req.params.order)
-        console.log(result)
-        if (result['json']===true) res.json(result)
-        else res.send(result)
-    }
-    catch(err){
-        console.log(err)
-        res.status(400).send('error!')
-    }
+app.group('/vrchat',VrcRouter)
+app.listen(PORT);
 
-});
-wsServer.on("connection", (ws: WebSocket) => {
-
-});
-
-server.listen(PORT, () => {
-    console.log(`Server is open... on: ${PORT}`);
-});
+//const wsServer = new WebSocket.Server({ server, path: "/cana_rin_lab_ws" });
+//wsServer.on("connection", (ws: WebSocket) => {});
--- src/vrchat/vrc_controller.ts
+++ src/vrchat/vrc_controller.ts
@@ -6,43 +6,23 @@
 import * as RinLib from './libproj/canarinlib' // Cana rin Lib project
 import * as RinWet from './weatherproj/canarinwet'
 import * as Utils from '../Utils/utils' // Formally.
+import {LocalHandler,ElysiaInstance,TypedSchema,Context,Handler,Elysia} from 'elysia' // Elysia
 export module VrcControl {
-    let db = Utils.DBpool
-    export async function routeVrcRequest(request:object,route:string):Promise<object>{
-        let result:object = {'ret':'','json':false}
-        switch(route) 
-        {
-            case 'test':
-                    result['ret'] = 'pong'
-                break;
-            case 'load_book':
-                let load_result = await RinLib.loadBook(request,db)
-                if (load_result['error'] == null)
-                    result['ret'] = load_result['bookinside']
-                else
-                    result['ret'] = 'there is no book on server!'+load_result['error']
-                break
-            case 'load_libs':
-                load_result = await RinLib.loadLibs(request,db)
-                if (load_result['error'] == null){
-                    result['books'] = load_result['libs']
-                    result['json']=true
-                }    
-                else
-                    result['ret'] ='there is no libs on server!'+load_result['error']
-                break
-            case 'get_weather':
-                load_result = await RinWet.getWeather(request)
-            default :
-                result['ret'] ='There is no command for server!'
-                break
-        }
-        
-        return result
-    }
-    async function insertUserData():Promise<object>
-    {
-        return 
-    }
+	let db = Utils.DBpool
+	//
+	export const checkVrcInfo = (context:LocalHandler<TypedSchema<never>, ElysiaInstance, "/vrchat">) => {
+
+	}
 
 }
+
+const VrcGroup = (c:{ body, set })=>{
+	return {}
+}
+
+export const VrcRouter = (app:Elysia <ElysiaInstance>) :Elysia<ElysiaInstance> => {
+	app
+	.get('/',VrcGroup)
+
+	return app;
+} 

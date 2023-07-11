22924ed603be49879b7c68effc89e038fdafc95f
Lutica_CANARD <80238084+LuticaCANARD@users.noreply.github.com>
2023-07-12 08:54:25 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { createServer } from "http";
 import { WebSocket } from "ws";
-import {VrcRouter} from "./vrchat/vrc_controller"
+import { VrcRouter } from "./vrchat/vrc_controller"
 //import * as WebControl from "./web/web_controller"
 import { PrismaClient } from '@prisma/client'
 import { Elysia,Context } from 'elysia'

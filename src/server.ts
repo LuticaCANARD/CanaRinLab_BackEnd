403466361440c3cbbb6bb6815f83abda9140608f
Lutica_CANARD <presan100@gmail.com>
2023-07-13 04:31:36 +0900

    aaaa

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,15 @@ import { VrcRouter } from "./vrchat/vrc_controller"
 import { Elysia,Context } from 'elysia'
 
 //const prisma = new PrismaClient()
-const PORT = Number(process.env.RIN_LAB_PORT) || 80; 
+const PORT = Number(process.env.RIN_LAB_PORT) || 10000; 
 
 const app = new Elysia()
 .get('/bb',()=>{return 'hi'})
 .group('/vrchat',VrcRouter)
 .listen({
 	port:PORT,
-	hostname:process.env.HOSTNAME || '0.0.0.0'});
+	//hostname:process.env.HOSTNAME || '0.0.0.0'
+});
 	console.log(process.env)
 
 console.log(`🦊 Elysia is running at ${app.server.hostname}:${app.server.port} / localhost:${app.server.port}`)

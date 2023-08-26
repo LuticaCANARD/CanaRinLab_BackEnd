7d7688846cf5bbd9efaa395c94a8345d2dff777b
Lutica_CANARD <presan100@gmail.com>
2023-08-26 20:10:46 +0900

    key

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,30 @@ import { DiscordRouter } from "./Discord/discordbotindex";
 //import * as WebControl from "./web/web_controller"
 //import { PrismaClient } from '@prisma/client'
 import { Elysia,Context } from 'elysia'
+import {readFileSync} from 'fs';
+import { buffer } from "stream/consumers";
 
 //const prisma = new PrismaClient()
 const PORT = Number(process.env.RIN_LAB_PORT) || 10000; 
+const tls:{
+	cert?:Buffer ,key?:Buffer
+}= {}
+if(!process.env.RIN_LAB_PORT)
+{
+	tls.cert = readFileSync('./keys/public.crt')
+	tls.key = readFileSync('./keys/private.key')
+}
 
 const app = new Elysia()
 .get('/',()=>{return 'hi'})
 .group('/vrchat',VrcRouter)
 .group('/discord',DiscordRouter)
 .listen({ 
-	port:443,  
-	hostname:process.env.HOSTNAME || '0.0.0.0'
-});
+	port:process.env.RIN_LAB_PORT||443,  
+	hostname:process.env.HOSTNAME || '0.0.0.0',
+	tls
+}
+);
 //console.log(process.env)
 
 console.log(`🦊 Elysia is running at ${app.server.hostname}:${app.server.port} / localhost:${app.server.port}`)

1f44c04a0c100bacb0beba1d66ce035d1e27d7ad
Lutica CANARD <presan100@gmail.com>
2023-09-18 23:04:45 +0900

    update

--- src/TYPES/types.ts
+++ src/TYPES/types.ts
@@ -2,6 +2,10 @@ import type { ColumnType } from "kysely";
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U> ? ColumnType<S, I | undefined, U> : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export type CasinoChat = {
+  id: number;
+  chatId: number;
+};
 export type CasinoEvent = {
   time: number;
   userId: number;
@@ -41,6 +45,7 @@ export type VoteExecutor = {
   UserId: number;
 };
 export type DB = {
+  CasinoChat: CasinoChat;
   CasinoEvent: CasinoEvent;
   CasinoMember: CasinoMember;
   CasinoRoles: CasinoRoles;

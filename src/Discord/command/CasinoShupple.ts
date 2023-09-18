28afecb3d14395792cc4ded87b73739e99d2e93c
Lutica CANARD <presan100@gmail.com>
2023-09-19 00:11:31 +0900

    dd

--- src/Discord/command/CasinoShupple.ts
+++ src/Discord/command/CasinoShupple.ts
@@ -6,15 +6,17 @@ module.exports= {
 		.setName('카지노 봅기')
 		.setDescription('카지노에 참가할 인원을 체크하고, 이 인원을 랜덤으로 배치합니다.'),
 	async execute(interaction:ChatInputCommandInteraction<CacheType>){
-		
-		const p = await interaction.reply({ content: '<everyone> 디음주 카지노 참가자 확인합니다!', fetchReply: true })
-		//interaction.channel?.fetch
-		await p.react('✅')
 		const read = await db.
 		selectFrom("CasinoChat")
-		.where("CasinoChat.id","=",Number(p.guildId))
+		.where("CasinoChat.id","=",Number(interaction.guildId))
 		.select("CasinoChat.chatId")
 		.execute();
+		const p = await interaction.reply({ content: '<everyone> 디음주 카지노 참가자 확인합니다!', fetchReply: true })
+		interaction.channel?.messages.fetch("701574160211771462")
+		
+		.then(message => console.log(message.content)).catch(console.error);
+		await p.react('✅')
+
 		if(read.length>0) await db.updateTable("CasinoChat").set({"chatId":Number(p.id)}).where("CasinoChat.id","=",Number(p.guildId)).execute();
 		else await db.insertInto("CasinoChat").values([{"id" : Number(p.guildId),"chatId":Number(p.id)}]).execute();
 	}

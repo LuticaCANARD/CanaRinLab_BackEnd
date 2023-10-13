fc758a3b343ce666710463c7c391579c3117bc4c
Lutica CANARD <presan100@gmail.com>
2023-10-13 20:39:54 +0900

    au_

--- src/Discord/au.ts
+++ src/Discord/au.ts
@@ -1,20 +1,20 @@
 import { db } from '../Utils/db';
 
-const meme_ = await db.selectFrom('CasinoMember').select('CasinoMember.userId').execute();
-const roles_ = await db.selectFrom('CasinoRoles').select('CasinoRoles.RoleName').orderBy('CasinoRoles.Priority').execute();
+const meme_ = await db.selectFrom('CasinoMember').select(['CasinoMember.userId','CasinoMember.name']).execute();
+const roles_ = await db.selectFrom('CasinoRoles').select(['CasinoRoles.RoleName']).orderBy('CasinoRoles.Priority').execute();
 let sttr = ''
 console.log('---')
-let meme_2 = meme_.map(c => c.userId)
-meme_2.sort(() => Math.random()-0.5) // 섞음
+meme_.sort(() => Math.random()-0.5) // 섞음
 
 const role_addt = new Map();
 let counter = 0;
 console.log('---')
 for(let rl of roles_){
 	const rname = rl["RoleName"];
-	const res_member = meme_2[counter];
-	if(res_member) sttr += `${rname} : ${res_member}\n`
-	if(counter > meme_2.length) break;
+	const res_member = meme_[counter];
+	console.log(res_member)
+	if(res_member) sttr += `${rname} : ${res_member.name}\n`
+	if(counter > meme_.length) break;
 	counter++;
 }
 console.log('---')

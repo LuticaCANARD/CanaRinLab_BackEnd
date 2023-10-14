0b278fd954d7cb1d762ef2e05a660c9f57a75ff1
Lutica CANARD <presan100@gmail.com>
2023-10-14 11:57:15 +0900

    masking

--- src/vrchat/vrc_controller.ts
+++ src/vrchat/vrc_controller.ts
@@ -40,14 +40,14 @@ const getPlayerHeader =  (c:Context<any,any>) =>{
 
 	if(v["x-forwarded-for"]!=undefined){
 		v["x-forwarded-for"] = hasher.update(v["x-forwarded-for"]).digest("base64");
-		
+		v["x-amzn-trace-id"] = "-";
+		v["host"] = "-";
 		c.set.headers["set-cookie"] = "key="+v["x-forwarded-for"]+';'
 
 	} else {
 		c.set.headers["set-cookie"] = "key="+hasher.update("local").digest("base64");+";"
 	}
-	// v["x-amzn-trace-id"] = "-";
-	// v["host"] = "-";
+	 
 	console.log(v);
 	// c.set.headers["set-cookie"] = "key="+String(SHA256.hash(v["x-forwarded-for"]))+";";
 	if(c.query["fail"]=="1") c.set.status = 400;
@@ -62,6 +62,8 @@ const ImageVRC = (c:Context<any,any>) =>{
 
 	if(v["x-forwarded-for"]!=undefined){
 		v["x-forwarded-for"] = hasher.update(v["x-forwarded-for"]).digest("base64");
+		v["x-amzn-trace-id"] = "-";
+		v["host"] = "-";
 		c.set.headers["set-cookie"] = "key="+v["x-forwarded-for"]+';'
 
 	} else {

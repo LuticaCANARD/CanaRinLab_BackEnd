75113790fac79a5b6c8c41e829aec23d364697e1
Lutica CANARD <presan100@gmail.com>
2023-11-02 14:44:21 +0900

    a

--- src/model/web.ts
+++ src/model/web.ts
@@ -1,6 +1,7 @@
 import {db} from '../Utils/db'
 
 export const saveRequest =async (username:string,user_contact:string,desc:string) => {
+    console.log('s')
     await db.insertInto("ProjectAnswer").values({name:username,contact:user_contact,desc}).execute();
 console.log('s')
     return await db.selectFrom("ProjectAnswer").select(["ProjectAnswer.id"]).where('ProjectAnswer.contact','=',user_contact).orderBy("ProjectAnswer.id","desc").limit(1).execute();

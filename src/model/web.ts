02ba6113e8a23608c6ccea6460cd93062d8f01d2
Lutica CANARD <presan100@gmail.com>
2023-11-02 14:41:11 +0900

    await problem

--- src/model/web.ts
+++ src/model/web.ts
@@ -3,7 +3,7 @@ import {db} from '../Utils/db'
 export const saveRequest =async (username:string,user_contact:string,desc:string) => {
     await db.insertInto("ProjectAnswer").values({name:username,contact:user_contact,desc}).execute();
 
-    return db.selectFrom("ProjectAnswer").select(["ProjectAnswer.id"]).where('ProjectAnswer.contact','=',user_contact).orderBy("ProjectAnswer.id","desc").limit(1).execute();
+    return await db.selectFrom("ProjectAnswer").select(["ProjectAnswer.id"]).where('ProjectAnswer.contact','=',user_contact).orderBy("ProjectAnswer.id","desc").limit(1).execute();
 }
 
 export const loadAnswer =async (id:Array<number>) => {
